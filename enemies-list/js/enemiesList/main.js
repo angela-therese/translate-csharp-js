70ab6434cf4d86e371944b3d78b665ed430f89a7
User-42 <a.t.iafrate@gmail.com>
2021-04-14 11:15:11 -0400

    completed

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,40 @@
-// Put your code here
+console.log("My Enemies List!")
+console.log("----------------")
+
+const enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: ["Being a jerk to me in elementary school", "Not being nice to me in elementary school"],
+        isReallyHated: true
+    },
+
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        offenses: ["Cut off Luke's hand", "Murdered all those kids", "Unkind management practices"],
+        isReallyHated: false
+    },
+
+    {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offenses: ["Phone calls in the theater", "Phone calls on the bus", "Phone calls in line at the grocery store"],
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        offenses: ["Keeps giving me a hotplate"],
+        isReallyHated: false
+    }
+]
+
+for(let i=0; i < enemies.length; i++){
+    if(enemies[i].isReallyHated === true){
+        console.log(enemies[i].firstName + " " + enemies[i].lastName + " (Really, really dislike!)")
+    }
+    else{
+        console.log(enemies[i].firstName + " " + enemies[i].lastName)
+    }
+}
